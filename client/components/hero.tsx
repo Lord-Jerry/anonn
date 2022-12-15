aafe28b93a59148942ee0cf29dd1f4b42d202170
Jeremiah Ajayi <lovinjerry004@gmail.com>
2022-12-16 00:57:14 +0100

    client: create hero component.

---
+++ client/components/hero.tsx
@@ -0,0 +1,16 @@
+
+export default function Hero() {
+	return (
+		<div>
+			<div className="bg-[url('/images/hero1.svg')] bg-cover bg-auto bg-no-repeat bg-center h-96" />
+			<div className="text-3xl mt-2 font-black text-center tracking-tight">
+				<h2>Masks on,</h2>
+				<h2>Chat as Anonn</h2>
+			</div>
+			<div className="mt-1 italic text-center text-base font-normal tracking-tight">
+				<h3>Nobody would know its you,</h3>
+				<h3>I promise!</h3>
+			</div>
+		</div>
+	);
+}
