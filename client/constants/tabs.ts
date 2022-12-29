229d3f51207725eaa19a33e4a07eee96c1d51fa7
Jeremiah Ajayi <lovinjerry004@gmail.com>
2022-12-29 15:45:43 +0100

    client: remove history tab, and implement message indicator on tabs.

--- client/constants/tabs.ts
+++ client/constants/tabs.ts
@@ -1,19 +1,22 @@
 import { Tab } from "types/tab";
 
 export const conversationTabs: Tab[] = [
-  {
-    id: 'rejected',
-    name: 'History',
-    selected: false,
-  },
+  // {
+  //   id: 'rejected',
+  //   name: 'History',
+  //   selected: false,
+  //   hasNewMessage: false,
+  // },
   {
     id: 'active',
     name: 'Ongoing',
     selected: true,
+    hasNewMessage: true,
   },
   {
     id: 'pending',
     name: 'Requests',
     selected: false,
+    hasNewMessage: true,
   },
 ];
