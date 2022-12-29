18d6724e55a27937df5dd233553540d9786f69b5
DamiCode18 <adebayoribukunoluwa@yahoo.com>
2022-12-29 19:09:21 +0100

    404 page

--- client/pages/404.tsx
+++ client/pages/404.tsx
@@ -1,14 +1,26 @@
 // 404.js
+import Navigation from 'components/Navigation'
 import Link from 'next/link'
 
 export default function FourOhFour() {
   return (
-    <div className="mx-auto p-8 text-sm">
-    <h1>Whatever it is you're looking for here, I seriously don't know...</h1>
-    <Link href="/conversations" className='underline'>
-        Get back home, ASAP!!!
+    <>
+    <Navigation title="Page not found!!!" />
+    <div className="flex justify-center items-center h-[100vh] w-full">
+  <div className="mx-auto px-8 text-md">
+    <h1 className="text-6xl">404</h1>
+    <h1 className='text-lg'>Whatever it is you're looking for here, I seriously don't know...</h1>
+    <h3 className="mt-6 text-xl">Go back to:</h3>
+    <Link href="/conversations" className='underline text-[#f8f886] m-2 text-sm'>
+        Conversations
+    </Link><br/>
+     <Link href="/profile" className='underline text-[#f8f886] m-2 text-sm'>
+        Profile
     </Link>
   </div>
+    </div>
+  
+  </>
   )
 
 }
