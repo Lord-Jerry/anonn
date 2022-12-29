6ad8fa437b86a24d02b82a222a71f08b1f4161e8
Jeremiah Ajayi <lovinjerry004@gmail.com>
2022-12-29 03:03:20 +0100

    client: last conversation button implementation.

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -87,7 +87,7 @@ export async function getServerSideProps(ctx: GetServerSidePropsContext) {
   return {
     props: {  
       isloggedIn: !!token,
-      username: currentUserUsername
+      username: currentUserUsername || '',
     },
   };
 }
