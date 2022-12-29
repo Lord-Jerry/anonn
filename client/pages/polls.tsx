258d827bbaf3ad034cb63f54e1b8c60e73c7a2e3
DamiCode18 <adebayoribukunoluwa@yahoo.com>
2022-12-29 18:56:48 +0100

    coming soon polls

---
+++ client/pages/polls.tsx
@@ -0,0 +1,13 @@
+import Navigation from "components/Navigation";
+import React from "react";
+
+export default function polls() {
+  return (
+    <>
+      <Navigation title="Polls" />
+      <div className="flex justify-center items-center h-[100vh] w-full">
+        <div className="mx-auto">Coming soon, lol</div>
+      </div>
+    </>
+  );
+}
