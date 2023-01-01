1bd973b4a242bd2fa55d8607cdb04143ad5ef539
DamiCode18 <adebayoribukunoluwa@yahoo.com>
2023-01-01 23:56:43 +0100

    refactored time, removed moment and replaced with dayjs

--- client/utils/useTime.ts
+++ client/utils/useTime.ts
@@ -1,13 +1,15 @@
-import moment from "moment";
+import dayjs from "dayjs";
 
 export const useTime = (time: Date) => {
-  if (moment(time).isSame(new Date(), "day")) {
-    return moment(time).format("hh:mm:a");
-  } else if (moment(time).isSame(new Date(), "week")) {
-    return moment(time).format("dddd");
-  } else if (moment(time).isSame(new Date(), "month")) {
-    return moment(time).format("ll");
+  const form = dayjs("2023-01-01").format("DD/MM/YYYY");
+  console.log(dayjs().isSame(form), "day", form);
+  if (dayjs().isSame("2023-01--1", "year")) {
+    return dayjs(time).format("hh:mm:A");
+  } else if (dayjs("2019-01-25T00:00:00-02:00Z").isSame(new Date(), "week")) {
+    return dayjs("2019-01-25T00:00:00-02:00Z").format("dddd");
+  } else if (dayjs("2019-01-25T00:00:00-02:00Z").isSame(new Date(), "month")) {
+    return dayjs("2019-01-25T00:00:00-02:00Z").format("ll");
   } else {
-    return moment(time).fromNow();
+    // return moment(time).fromNow();
   }
 };
