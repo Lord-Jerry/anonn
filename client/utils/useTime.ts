79e7cf6a7afd32076151e5a8d4869e5afb02935b
DamiCode18 <adebayoribukunoluwa@yahoo.com>
2023-01-02 23:14:14 +0100

    dayjs isSameDay display

--- client/utils/useTime.ts
+++ client/utils/useTime.ts
@@ -1,11 +1,11 @@
 import dayjs from "dayjs";
 
 export const useTime = (time: Date) => {
-  const form = dayjs("2023-01-01").format("DD/MM/YYYY");
-  console.log(dayjs().isSame(form), "day", form);
-  if (dayjs().isSame("2023-01--1", "year")) {
-    return dayjs(time).format("hh:mm:A");
-  } else if (dayjs("2019-01-25T00:00:00-02:00Z").isSame(new Date(), "week")) {
+  const form = dayjs(time);
+  console.log(+form.format("D"));
+  if (new Date().getDate() === +form.format("D")) {
+    return dayjs(time).format("hh:mm: A");
+  } else if (new Date().getDate() - 7 === +form.format("D")) {
     return dayjs("2019-01-25T00:00:00-02:00Z").format("dddd");
   } else if (dayjs("2019-01-25T00:00:00-02:00Z").isSame(new Date(), "month")) {
     return dayjs("2019-01-25T00:00:00-02:00Z").format("ll");
