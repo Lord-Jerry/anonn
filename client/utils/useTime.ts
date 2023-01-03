621effcba78b3b4e0b7975e22798248fe34fdee9
DamiCode18 <adebayoribukunoluwa@yahoo.com>
2023-01-03 11:48:08 +0100

    dayjs date format

--- client/utils/useTime.ts
+++ client/utils/useTime.ts
@@ -2,14 +2,10 @@ import dayjs from "dayjs";
 
 export const useTime = (time: Date) => {
   const form = dayjs(time);
-  console.log(+form.format("D"));
+  console.log(new Date().getFullYear(), +form.get("year"));
   if (new Date().getDate() === +form.format("D")) {
     return dayjs(time).format("hh:mm: A");
-  } else if (new Date().getDate() - 7 === +form.format("D")) {
-    return dayjs("2019-01-25T00:00:00-02:00Z").format("dddd");
-  } else if (dayjs("2019-01-25T00:00:00-02:00Z").isSame(new Date(), "month")) {
-    return dayjs("2019-01-25T00:00:00-02:00Z").format("ll");
   } else {
-    // return moment(time).fromNow();
+    return dayjs(time).format("MMM D, YYYY");
   }
 };
