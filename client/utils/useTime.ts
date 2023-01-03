cbf89b537596d710b5667414d9ac82ab121f0552
DamiCode18 <adebayoribukunoluwa@yahoo.com>
2023-01-03 11:48:56 +0100

    dayjs date format

--- client/utils/useTime.ts
+++ client/utils/useTime.ts
@@ -2,7 +2,6 @@ import dayjs from "dayjs";
 
 export const useTime = (time: Date) => {
   const form = dayjs(time);
-  console.log(new Date().getFullYear(), +form.get("year"));
   if (new Date().getDate() === +form.format("D")) {
     return dayjs(time).format("hh:mm: A");
   } else {
